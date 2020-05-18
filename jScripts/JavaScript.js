6b4fce43b9538e2acf9dc510096b1dafc382ad5e
talyakotler <50055296+talyakotler@users.noreply.github.com>
2020-05-18 22:34:13 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,21 @@
+﻿var gameHeight;
+var gameWidth;
+var viewportHeight = $(window).height();
+var viewportWidth = $(window).width();
+
+$(window).load(function () {
+    gameHeight = $("#animation_container").height();
+    gameWidth = $("#animation_container").width();
+})
+
+function resize() {
+    var scale = Math.min(viewportWidth / gameWidth, viewportHeight / gameHeight) + 0.05;
+    console.log(parseFloat(scale.toFixed(2)));
+    $("#animation_container").css("transform", "scale(" + scale + ")");
+    console.log(gameHeight);
+    console.log(gameWidth);
+    console.log(viewportWidth);
+    console.log(viewportHeight);
+}
+
+
